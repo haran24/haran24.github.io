4d698ee400fdfebb16c0e383af5c86eb99c4b824
Haran Jeyachandra <32371531+haran24@users.noreply.github.com>
2019-11-03 13:06:47 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+
+///
+//fade out animation
+///
+
+$(document).ready(function() {
+  "use strict";
+
+  var c,
+    currentScrollTop = 0,
+    navbar = $("nav");
+
+  $(window).scroll(function() {
+    var a = $(window).scrollTop();
+    var b = navbar.height();
+
+    currentScrollTop = a;
+
+    if (c < currentScrollTop && a > b + b) {
+      navbar.addClass("scrollUp");
+    } else if (c > currentScrollTop && !(a <= b)) {
+      navbar.removeClass("scrollUp");
+    }
+    c = currentScrollTop;
+  });
+});
+
+//hide nav on scroll down
+var new_scroll_position = 0;
+var last_scroll_position;
+var header = document.getElementById("header");
+
+window.addEventListener('scroll', function(e) {
+  last_scroll_position = window.scrollY;
+
+  // Scrolling down
+  if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+    // header.removeClass('slideDown').addClass('slideUp');
+    header.classList.remove("slideDown");
+    header.classList.add("slideUp");
+
+  // Scrolling up
+  } else if (new_scroll_position > last_scroll_position) {
+    // header.removeClass('slideUp').addClass('slideDown');
+    header.classList.remove("slideUp");
+    header.classList.add("slideDown");
+  }
+
+  new_scroll_position = last_scroll_position;
+});
+
+
+//go to section nav
+$("nav").find("section").click(function(e) {
+  e.preventDefault();
+  var section = $(this).attr("href");
+  $("html, body").animate({
+      scrollTop: $(section).offset().top
+  });
+});
